4ecd7cddd623517a4a3808b007a4cbcafc2a6e04
Jackma3 <1805650352@qq.com>
2021-03-11 11:50:17 +0800

    Update train.py

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from config.metric import get_ner_fmeasure as measure
 from config.config import params as hp
 from model.Bert_lstm import Bertencoder
 from pro_data.get_data import load_data
+from torch.nn import DataParallel
 from config.AdamW import AdamW
 from torch.optim import Adam, SGD
 import torch
@@ -10,7 +11,7 @@ import torch
 def train():
 
     use_gpu = torch.cuda.is_available()
-    device = torch.device('cuda:5' if use_gpu else 'cpu')
+    device = torch.device('cuda:0' if use_gpu else 'cpu')
 
     # load train data
     train_data = load_data(hp.batch_size, use_gpu, 'train')
@@ -24,6 +25,8 @@ def train():
     # model
     model = Bertencoder(hp.bert_dim, hp.output_dim)
     if use_gpu:
+        # use more gpu train model
+        #model = Dataparallel(model, device_ids=[0, 1, 2, 3])
         model.to(device)
 
     # define optimizer
@@ -37,6 +40,7 @@ def train():
             if use_gpu:
                 train_x[j], train_y[j] = train_x[j].to(device), train_y[j].to(device)
                 train_seg_ids[j], train_mask[j] = train_seg_ids[j].to(device), train_mask[j].to(device)
+            #loss = model(train_x[j], train_y[j], train_seg_ids[j], train_mask[j]).mean()                # dataparallel
             loss = model(train_x[j], train_y[j], train_seg_ids[j], train_mask[j])
             optimizer.zero_grad()
             loss.backward()
@@ -54,10 +58,13 @@ def train():
                 test_x[h], test_y[h] = test_x[h].to(device), test_y[h].to(device)
                 test_seg_ids[h], test_mask[h] = test_seg_ids[h].to(device), test_mask[h].to(device)
             with torch.no_grad():
+                #loss, pre_y = model.module.test(test_x[h], test_y[h], test_seg_ids[h], test_mask[h])     # dataparallel
                 loss, pre_y = model.test(test_x[h], test_y[h], test_seg_ids[h], test_mask[h])
+            #dev_loss += loss.mean().item()        # dataparallel
             dev_loss += loss.item()
 
             predict_idx_label.append(pre_y)
+            #print('test_iteration:\t{}\t\tloss:\t{}'.format(h+1, round(loss.mean().item(), 4)))          # dataparallel
             print('test_iteration:\t{}\t\tloss:\t{}'.format(h+1, round(loss.item(), 4)))
 
         predict_label = test_data.trans2tag(predict_idx_label)
