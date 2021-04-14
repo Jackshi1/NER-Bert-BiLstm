9fc77d8f51b868d1bae7bf545bf1efe8cee9dd09
Jackma3 <1805650352@qq.com>
2021-04-14 12:40:37 +0800

    Update Bert_lstm.py

--- model/Bert_lstm.py
+++ model/Bert_lstm.py
@@ -4,10 +4,11 @@ from torch import nn
 
 
 class Bertencoder(nn.Module):
-    def __init__(self, bert_dim, output_dim):
+    def __init__(self, bert_dim, output_dim, num_layers, rnn_dim):
         super(Bertencoder, self).__init__()
         self.bert_model = AutoModel.from_pretrained(bert_model_path)
-        self.linear = nn.Linear(bert_dim, output_dim)
+        self.rnn = nn.LSTM(bert_dim, rnn_hidden, num_layers=num_layers, batch_first=True, bidirectional=True)
+        self.linear = nn.Linear(rnn_dim*2, output_dim)
         self.loss_function = nn.CrossEntropyLoss()
 
     def forward(self, x, y, seg_ids, mask):
@@ -17,14 +18,16 @@ class Bertencoder(nn.Module):
         """
 
         bert_output = self.bert_model(x, token_type_ids=seg_ids, attention_mask=mask)
-        output = self.linear(bert_output.last_hidden_state).transpose(1, 2)
+        lstm_output, _ = self.rnn(bert_output.last_hidden_state)
+        output = self.linear(lstm_output).transpose(1, 2)
         loss = self.loss_function(output, y)
         return loss
 
     def test(self, x, y, seg_ids, mask):
 
         bert_output = self.bert_model(x, token_type_ids=seg_ids, attention_mask=mask)
-        output = self.linear(bert_output.last_hidden_state).transpose(1, 2)
+        lstm_output, _ = self.rnn(bert_output.last_hidden_state)
+        output = self.linear(lstm_output).transpose(1, 2)
         loss = self.loss_function(output, y)
         predict = self.predict_label(output.transpose(1, 2), mask)
         return loss, predict
